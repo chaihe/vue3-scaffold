18cc8fe2736c4cadad1ac440268b5d908f73b7f2
Chai <80431992@qq.com>
2022-05-05 00:56:59 +0800

    feat: 获取env环境变量为对应值

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import alias from './vite/alias'
+import { parseEnv } from './vite/util'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias,
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias,
+//   }
+// })
+
+export default ({ command, mode }: ConfigEnv) => {
+  const isBuild = command === 'build'
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  console.log(parseEnv(env));
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias,
+    }
   }
-})
+
+}
--- vite/alias.ts
+++ vite/alias.ts
@@ -2,7 +2,8 @@ import { resolve } from 'path'
 import { AliasOptions } from 'vite'
 
 const alias = {
-  '@': resolve(__dirname, '/src')
+  '@': resolve(__dirname, '../src'),
+  '#': resolve(__dirname, '../types'),
 } as AliasOptions
 
 export default alias
